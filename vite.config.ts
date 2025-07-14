0ab8f73bb56d5f2cf5a4fd1256e43d5c2710f950
Thuc tap sinh 5 <92751063+WilllamAbus@users.noreply.github.com>
2025-07-14 12:31:40 +0700

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(async () => {
   const { default: checker } = await import('vite-plugin-checker');
   return {
-    plugins: [react(), checker({ typescript: true })],
-      server: {
-    open: true,
-  },
+    base: "/", // Quan trọng khi build để Netlify hiểu đúng đường dẫn assets
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+    ],
+    server: {
+      open: true, // Chỉ dùng cho môi trường local dev
+    },
+    // build: {
+    //   outDir: "dist", // Thư mục Netlify sử dụng
+    // },
   };
 });
+
